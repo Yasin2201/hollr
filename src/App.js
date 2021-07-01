08c8912030c1010ac605d5110e899868cd50a792
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-07-01 21:59:50 +0100

    setup firebase in app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import firebase from "./components/firebase";
+
 function App() {
+
+  const db = firebase.firestore()
+
+  const getData = async () => {
+    const data = db.collection('test')
+    const ref = await data.get()
+
+    ref.docs.map((doc) => console.log(doc.data()))
+  }
+
+
   return (
     <div>
       <h1>holl'r</h1>
+      <button onClick={getData}>click</button>
     </div>
   );
 }
---
+++ src/components/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBukyt4EroXxaVMBVOP7Nh6uYwfdpqY3WE",
+    authDomain: "holl-r.firebaseapp.com",
+    projectId: "holl-r",
+    storageBucket: "holl-r.appspot.com",
+    messagingSenderId: "8271554529",
+    appId: "1:8271554529:web:4376a6890037eb6d6294a0"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase
