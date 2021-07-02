9d80058d0a4001973ea957b1fb680c5b4b02c2de
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-07-02 22:31:21 +0100

    push data to database

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -9,11 +9,22 @@ const Posts = ({ userUID }) => {
         setPostData(e.target.value)
     }
 
+    const submitPost = (e) => {
+        e.preventDefault()
 
+        db.collection('Posts').doc().set({
+            user: userUID,
+            data: postData,
+            datePosted: firebase.firestore.FieldValue.serverTimestamp()
+        })
+
+        setPostData('')
+        e.target.reset()
+    }
 
     return (
         <div>
-            <form>
+            <form onSubmit={submitPost}>
                 <input type='text' onChange={addData} />
                 {postData.length < 1 ? <button type='submit' disabled>Post</button> : <button type='submit'>Post</button>}
             </form>
