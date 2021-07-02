4a334d4fe522683b1fa12152990f63ed6b869f25
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-07-02 21:28:04 +0100

    added account/user authentication

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import firebase from "./components/firebase";
+import Login from "./components/Login";
 
 function App() {
 
   const db = firebase.firestore()
+  console.log(db)
+  // const getData = async () => {
+  //   const data = db.collection('test')
+  //   const ref = await data.get()
 
-  const getData = async () => {
-    const data = db.collection('test')
-    const ref = await data.get()
-
-    ref.docs.map((doc) => console.log(doc.data()))
-  }
+  //   ref.docs.map((doc) => console.log(doc.data()))
+  // }
 
 
   return (
     <div>
       <h1>holl'r</h1>
-      <button onClick={getData}>click</button>
+      <Login />
     </div>
   );
 }
---
+++ src/components/Login.js
@@ -0,0 +1,43 @@
+import firebase from "firebase";
+import { useEffect, useState } from "react";
+import { useAuthState } from 'react-firebase-hooks/auth';
+
+const Login = () => {
+    const [username, setUsername] = useState(undefined)
+    const auth = firebase.auth()
+    const [user] = useAuthState(auth)
+
+    const signIn = () => {
+        const provider = new firebase.auth.GoogleAuthProvider();
+        auth.signInWithPopup(provider);
+    }
+
+    const signOut = () => {
+        auth.signOut()
+    }
+
+    const getUsername = () => {
+        setUsername(user.displayName)
+    }
+
+    useEffect(() => {
+        if (user !== null) {
+            getUsername()
+        }
+    })
+
+    return (
+        <div>
+            {user
+                ? <div>
+                    <h3>Hello, {username}!</h3>
+                    <button onClick={signOut}>Sign Out</button>
+                </div>
+                : <div>
+                    <button onClick={signIn}>Sign In</button>
+                </div>}
+        </div>
+    )
+}
+
+export default Login
