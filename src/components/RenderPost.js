9d597ade718427073d0f94423f3669d5d9f7a878
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-07-03 23:10:59 +0100

    add comment button and state

--- src/components/RenderPost.js
+++ src/components/RenderPost.js
@@ -1,17 +1,29 @@
 import firebase from "firebase";
+import { useState } from "react";
+import Comment from "./Comment";
 
 const RenderPost = ({ post, userUID }) => {
+    const [comment, setComment] = useState(false)
 
     const deletePost = () => {
         const db = firebase.firestore()
         db.collection('Posts').doc(post.id).delete()
     }
 
+    const changeCommentState = () => {
+        setComment(!comment)
+    }
     return (
         <div key={post.id} style={{ border: '1px solid black' }}>
             <p>{post.username}</p>
             <p>{post.data}</p>
             {userUID === post.userID && <button onClick={deletePost}>Delete</button>}
+            {comment ?
+                <div>
+                    <Comment />
+                    <button onClick={changeCommentState}>cancel</button>
+                </div>
+                : <button onClick={changeCommentState}>Comment</button>}
         </div>
     )
 }
