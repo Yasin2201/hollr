c4d69934da7291a6cde01fa90909262ee0b92e01
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-07-05 23:35:50 +0100

    render Profile when routed

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,6 +1,27 @@
-const Profile = ({ userInfo }) => {
+import { useEffect, useState } from "react"
+
+const Profile = ({ navigateProfile, allPosts, allComments }) => {
+    const [loading, setLoading] = useState(false)
+    const [profileName, setProfileName] = useState()
+    const [profilesPosts, setProfilesPosts] = useState()
+    const [profilesComments, setProfilesComments] = useState()
+
+    useEffect(() => {
+        setProfileName(allPosts.find((user) => user.userID === navigateProfile).displayName)
+        setProfilesPosts(allPosts.filter((post) => post.userID === navigateProfile))
+        setProfilesComments(allComments.filter((comment) => comment.userID === navigateProfile))
+        setLoading(true)
+    }, [navigateProfile, allPosts, allComments])
+
+    console.log(profilesPosts)
+    console.log(profilesComments)
     return (
-        <div> {userInfo.displayName}</div>
+        <div>
+            {loading
+                ? <div>{profileName}</div>
+                : <h2>loading...</h2>
+            }
+        </div>
     )
 }
 
