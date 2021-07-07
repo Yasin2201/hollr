573f84ed34df86d14b87fb43d85659fc25fd882e
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-07-07 17:17:42 +0100

    added edit post functionality

--- src/components/Posts/RenderPost.js
+++ src/components/Posts/RenderPost.js
@@ -7,10 +7,27 @@ import RenderComments from "../Comments/RenderComments";
 const RenderPost = ({ currUser, post, userInfo, allComments, navigateToProfile }) => {
     const [comment, setComment] = useState(false)
     const [showReplies, setShowReplies] = useState(false)
+    const [edit, setEdit] = useState(false)
+    const [editData, setEditData] = useState('')
 
-    const deletePost = () => {
+    const onDelete = () => {
         const db = firebase.firestore()
         db.collection('Posts').doc(post.id).delete()
+    }
+
+    const onEdit = () => {
+        setEdit(!edit)
+    }
+
+    const addEditData = (e) => {
+        setEditData(e.target.value)
+    }
+
+    const onEditSubmit = () => {
+        const db = firebase.firestore();
+        db.collection("Posts").doc(post.id).update({ data: editData });
+        setEditData('')
+        setEdit(!edit)
     }
 
     const changeCommentState = () => {
@@ -25,7 +42,24 @@ const RenderPost = ({ currUser, post, userInfo, allComments, navigateToProfile }
         <div key={post.id} style={{ border: '1px solid black' }}>
             <Link to={`/profile/${post.userID}`} onClick={navigateToProfile} id={post.userID}>{post.displayName}</Link>
             <p>{post.data}</p>
-            {currUser.uid === post.userID && <button onClick={deletePost}>Delete</button>}
+
+            {
+                currUser.uid === post.userID &&
+                <div>
+                    <button onClick={onDelete}>Delete</button>
+                    {
+                        edit
+                            ? <div>
+                                <button onClick={onEdit}>Cancel Edit</button>
+                                <input type='text' onChange={addEditData} />
+                                <button onClick={onEditSubmit}>Submit Edit</button>
+                            </div>
+                            : <button onClick={onEdit}>Edit</button>
+                    }
+
+                </div>
+            }
+
             {comment ?
                 <div>
                     <button onClick={changeCommentState}>cancel</button>
