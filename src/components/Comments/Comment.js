85051be3daf3f2910dd79fec09db4df84298bb7e
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-07-07 17:27:19 +0100

    added edit comments on posts functionality

--- src/components/Comments/Comment.js
+++ src/components/Comments/Comment.js
@@ -1,10 +1,28 @@
 import firebase from "firebase";
+import { useState } from "react";
 
 const Comment = ({ commentInfo, currentUser }) => {
+    const [edit, setEdit] = useState(false)
+    const [editData, setEditData] = useState('')
 
-    const deletePost = () => {
+    const onDelete = () => {
         const db = firebase.firestore()
         db.collection('Comments').doc(commentInfo.id).delete()
+    }
+
+    const onEdit = () => {
+        setEdit(!edit)
+    }
+
+    const addEditData = (e) => {
+        setEditData(e.target.value)
+    }
+
+    const onEditSubmit = () => {
+        const db = firebase.firestore();
+        db.collection("Comments").doc(commentInfo.id).update({ data: editData });
+        setEditData('')
+        setEdit(!edit)
     }
 
     return (
@@ -12,7 +30,23 @@ const Comment = ({ commentInfo, currentUser }) => {
             <div key={commentInfo.id} style={{ border: '1px solid black' }}>
                 <p>{commentInfo.displayName}</p>
                 <p>{commentInfo.data}</p>
-                {currentUser.uid === commentInfo.userID && <button onClick={deletePost}>Delete</button>}
+
+                {
+                    currentUser.uid === commentInfo.userID &&
+                    <div>
+                        <button onClick={onDelete}>Delete</button>
+                        {
+                            edit
+                                ? <div>
+                                    <button onClick={onEdit}>Cancel Edit</button>
+                                    <input type='text' onChange={addEditData} />
+                                    <button onClick={onEditSubmit}>Submit Edit</button>
+                                </div>
+                                : <button onClick={onEdit}>Edit</button>
+                        }
+
+                    </div>
+                }
             </div>
         </div>
     )
