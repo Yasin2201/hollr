64ffae63bf18d3f741f722c9573305d882d0dcf0
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-07-08 19:07:15 +0100

    get profile following info from firestore

--- src/components/Profile/Following.js
+++ src/components/Profile/Following.js
@@ -7,6 +7,19 @@ const Following = ({ allUsers, currUser, currProfile }) => {
     // const [followers, setFollowers] = useState()
     const db = firebase.firestore()
 
+    useEffect(() => {
+        const unsubscribe = db.collection("Users").doc(currProfile.uid)
+            .collection('Following').onSnapshot((querySnapshot) => {
+                const following = []
+                querySnapshot.forEach(doc => {
+                    following.push(doc.id)
+                });
+                setFollowing(following)
+            });
+        return unsubscribe
+
+    }, [db, currProfile])
+
 
     return (
         <div>
