d76b3ad12e3f8ace1055b54b0a763c1c86d61df9
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-07-13 21:22:44 +0100

    added sidebar styling

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,10 +1,17 @@
 import { Link } from "react-router-dom"
+import '../Styles/Sidebar.css'
 
 const Sidebar = ({ navigateToProfile, user }) => {
     return (
-        <div>
-            <Link to={`/profile/${user.uid}`}>
-                <button id={user.uid} onClick={navigateToProfile}>My Profile</button>
+        <div id='sidebar'>
+            <Link to={`/`} className='sideBarBtns'>
+                Home
+            </Link>
+            <Link to={`/`} className='sideBarBtns'>
+                Github
+            </Link>
+            <Link to={`/profile/${user.uid}`} className='sideBarBtns' id={user.uid} onClick={navigateToProfile}>
+                Your Profile
             </Link>
         </div>
     )
