737639b6ae14624ab58abe16e4471a8932ec377c
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-07-15 22:49:23 +0100

    added icons to sidebar panel buttons

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,18 +1,33 @@
 import { Link } from "react-router-dom"
 import '../Styles/Sidebar.css'
+import homeIcon from '../Assets/home.svg'
+import githubIcon from '../Assets/github.svg'
+import userIcon from '../Assets/user.svg'
 
 const Sidebar = ({ navigateToProfile, user }) => {
     return (
         <div id='sidebar'>
-            <Link to={`/`} className='sideBarBtns'>
-                Home
-            </Link>
-            <Link to={`/`} className='sideBarBtns'>
-                Github
-            </Link>
-            <Link to={`/profile/${user.uid}`} className='sideBarBtns' id={user.uid} onClick={navigateToProfile}>
-                Your Profile
-            </Link>
+            <div className='sidebar-button-panel'>
+                <img src={homeIcon} alt="home" className='sidebar-icon' />
+                <Link to={`/`} className='sideBarBtns'>
+                    Home
+                </Link>
+            </div>
+
+            <div className='sidebar-button-panel'>
+                <img src={githubIcon} alt="github" className='sidebar-icon' />
+                <Link to={`/`} className='sideBarBtns'>
+                    Github
+                </Link>
+            </div>
+
+            <div className='sidebar-button-panel'>
+                <img src={userIcon} alt="profile" className='sidebar-icon' />
+                <Link to={`/profile/${user.uid}`} className='sideBarBtns' id={user.uid} onClick={navigateToProfile}>
+                    Your Profile
+                </Link>
+            </div>
+
         </div>
     )
 }
