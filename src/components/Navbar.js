e7f1370442c5bdb0e202f9707870acc60774a82f
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-07-15 22:56:30 +0100

    added logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,13 @@
 import { Link } from "react-router-dom"
 import './Styles/Navbar.css'
-
+import logo from './Assets/logo.svg'
 const Navbar = ({ user, signOut }) => {
     return (
         <div id='navbar'>
             <Link to='/' className='title'>
                 <h1>holl'r</h1>
             </Link>
+            <img src={logo} alt='hollr logo' className='logo' />
             <div id='navActions'>
                 <h3 className='greeting'>Hello, {user.displayName}!</h3>
                 <button className='signOut' onClick={signOut}>Sign Out</button>
