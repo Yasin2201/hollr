30af984d7602616e486c1d86326ee83d8ded36f7
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-07-16 14:57:51 +0100

    make logo click navigate to home

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,9 @@ const Navbar = ({ user, signOut }) => {
             <Link to='/' className='title'>
                 <h1>holl'r</h1>
             </Link>
-            <img src={logo} alt='hollr logo' className='logo' />
+            <Link to='/' className='title'>
+                <img src={logo} alt='hollr logo' className='logo' />
+            </Link>
             <div id='navActions'>
                 <h3 className='greeting'>Hello, {user.displayName}!</h3>
                 <button className='signOut' onClick={signOut}>Sign Out</button>
